072e2ac2f26e4fcc22d9b97e054bf00e675858f5
moshabrawy <mohamedshabrawy@affsquare.com>
2023-03-31 03:29:22 +0200

    edit hover , owl-nav for specialties section

--- projects/user/src/app/views/page/home/specialties/specialties.component.ts
+++ projects/user/src/app/views/page/home/specialties/specialties.component.ts
@@ -3,29 +3,30 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-specialties',
   templateUrl: './specialties.component.html',
-  styleUrls: ['./specialties.component.scss']
+  styleUrls: ['./specialties.component.scss'],
 })
 export class SpecialtiesComponent {
   customOptions: OwlOptions = {
     loop: true,
-    margin:15,
+    margin: 15,
     autoplay: true,
     autoplayTimeout: 7000,
     nav: true,
-    dots:false,
+    dots: false,
     navText: [
-        "<span style='display:none'></span><i class='bx bx-left-arrow-circle'data-toggle='tooltip' title='previous'></i>",
-        "<span style='display:none'></span><i class='bx bx-right-arrow-circle'data-toggle='tooltip' title='next'></i> ",],
+      "<i class='bx bx-chevron-left' data-toggle='tooltip' title='previous'></i>",
+      "<i class='bx bx-chevron-right' data-toggle='tooltip' title='next'></i>",
+    ],
     responsive: {
-        0: {
-            items: 2,
-        },
-        600: {
-            items: 2,
-        },
-        1000: {
-            items:4,
-        },
+      0: {
+        items: 2,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 4,
+      },
     },
-  }
+  };
 }
