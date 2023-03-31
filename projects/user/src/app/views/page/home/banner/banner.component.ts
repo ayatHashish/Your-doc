a0efa1f7aaa77f841693c6a503613104a54c5336
moshabrawy <mo.shabrawydev@gmail.com>
2023-03-31 03:38:53 +0200

    bannar owl-av for banner section

--- projects/user/src/app/views/page/home/banner/banner.component.ts
+++ projects/user/src/app/views/page/home/banner/banner.component.ts
@@ -6,27 +6,28 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss'],
 })
-export class BannerComponent{
+export class BannerComponent {
   customOptions: OwlOptions = {
     loop: true,
     margin: 0,
     autoplay: true,
     autoplayTimeout: 7000,
     nav: true,
-    dots:false,
+    dots: false,
     navText: [
-        "<span style='display:none'></span><i class='bx bx-left-arrow-circle'data-toggle='tooltip' title='previous'></i>",
-        "<span style='display:none'></span><i class='bx bx-right-arrow-circle'data-toggle='tooltip' title='next'></i> ",],
+      "<i class='bx bx-chevron-left' data-toggle='tooltip' title='previous'></i>",
+      "<i class='bx bx-chevron-right' data-toggle='tooltip' title='next'></i>",
+    ],
     responsive: {
-        0: {
-            items: 1,
-        },
-        600: {
-            items: 1,
-        },
-        1000: {
-            items: 1,
-        },
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
     },
-  }
+  };
 }
