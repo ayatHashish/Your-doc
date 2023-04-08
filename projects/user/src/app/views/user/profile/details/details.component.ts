03c7c349aa020b69cd192d2cd1b2a3880a088b55
ayatHashish <76957743+ayatHashish@users.noreply.github.com>
2023-04-09 01:46:06 +0200

    share data between 2  components

--- projects/user/src/app/views/user/profile/details/details.component.ts
+++ projects/user/src/app/views/user/profile/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-details',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent {
-
+  @Input() profileDetails: any;
 }
--- projects/user/src/app/views/user/profile/setting/setting.component.ts
+++ projects/user/src/app/views/user/profile/setting/setting.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,Input  } from '@angular/core';
 
 @Component({
   selector: 'app-setting',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./setting.component.scss','./../details/details.component.scss'],
 })
 export class SettingComponent {
-
+ 
 }
