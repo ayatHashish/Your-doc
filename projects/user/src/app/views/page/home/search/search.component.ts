76588636ddeced6591c4a2d8d0b444e6b51c216d
moshabrawy <mo.shabrawydev@gmail.com>
2023-04-25 09:42:44 +0200

    search

--- projects/user/src/app/views/page/home/search/search.component.ts
+++ projects/user/src/app/views/page/home/search/search.component.ts
@@ -6,33 +6,44 @@ import { DoctorsService } from 'projects/user/src/app/share/services/doctors.ser
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
+
 export class SearchComponent {
-  // <option value="{{specialty.id}}"  *ngFor ="let specialty of specialties">{{specialty.title}}</option>
-  nameDoctor : any
-  specialties:any
-  constructor(private _search: DoctorsService, private _router: Router,  private _specialty:DoctorsService) {
-    this.search()
-    this. getAllSpecialties()
+  nameDoctor: any;
+  specialties: any;
+  errorMsg = '';
+  constructor(
+    private _search: DoctorsService,
+    private _router: Router,
+    private _specialty: DoctorsService
+  ) {
+    this.getAllSpecialties();
   }
-  searchForm =new FormGroup({
-    doctor_name : new FormControl('', [Validators.required, Validators.email]),
+
+  searchForm = new FormGroup({
+    doctor_name: new FormControl('', [Validators.required]),
     specialty_id: new FormControl('', [Validators.required]),
-  })
-  search(){
-    this._search.search(this.searchForm.value).subscribe(
-      (res)=>{
-this.nameDoctor = res.data
-console.log(res.data)
-},
-()=> this._router.navigateByUrl('/profile')
+  });
+  get doctor_name(){return this.searchForm.get('doctor_name')}
 
-   )}
-   getAllSpecialties(){
+  search() {
+    if(this.searchForm.valid){
+      this._search.search(this.searchForm.value).subscribe(
+        (res) => {
+          this.nameDoctor = res.data;
+          console.log(res.data);
+        },
+        () => this._router.navigateByUrl('/profile')
+      );
+    }else{
+      this.errorMsg = 'You should type docotr name and choose his specialist.'
+    }
+  }
+
+  getAllSpecialties() {
     this._specialty.allSpatialists().subscribe((res) => {
-      this.specialties = res.data
+      this.specialties = res.data;
     });
-
   }
 }
--- projects/user/src/app/views/user/profile/setting/setting.component.ts
+++ projects/user/src/app/views/user/profile/setting/setting.component.ts
@@ -1,11 +1,14 @@
-import { Component ,Input  } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ProfileService } from 'projects/user/src/app/share/services/profile.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-setting',
   templateUrl: './setting.component.html',
-  styleUrls: ['./setting.component.scss','./../details/details.component.scss'],
+  styleUrls: [
+    './setting.component.scss',
+    './../details/details.component.scss',
+  ],
 })
 export class SettingComponent {
   @Input() profilesetting: any;
@@ -21,14 +24,13 @@ export class SettingComponent {
   divtoggletext: boolean = true;
   imagediplay: any;
 
-  constructor(private _update: ProfileService, private _router: Router) { }
+  constructor(private _update: ProfileService, private _router: Router) {}
 
   onchangeInput(e: any) {
     e.stopPropagation();
     this.updateDataJson[`${e.target.name}`] = e.target.value;
     console.log(this.updateDataJson);
   }
-
 
   updatedForm: FormGroup = new FormGroup({
     first_name: new FormControl(null, [Validators.required]), // form control in angular mean input
@@ -44,55 +46,44 @@ export class SettingComponent {
     address: new FormControl(null, [Validators.required]),
     state: new FormControl(null, [Validators.required]),
     country: new FormControl(null, [Validators.required]),
-
     // password: this.password,
     // password_confirmation: this.password_confirmation,
+  });
 
+  // getuserdata() {
+  //   this._AuthService.getuserdata().subscribe((res) => {
+  //     this.userdata = res.data;
+  //     this.imagediplay = res.data[0].image;
+  //     this.onedit();
+  //   });
+  // }
+  //edit for make data at input to update it
+  onedit() {
+    this.updatedForm.controls['first_name'].setValue(
+      this.userdata[0]?.first_name
+    );
+    this.updatedForm.controls['last_name'].setValue(
+      this.userdata[0]?.last_name
+    );
+    this.updatedForm.controls['phone'].setValue(this.userdata[0]?.phone);
+    this.updatedForm.controls['email'].setValue(this.userdata[0]?.email);
+    this.updatedForm.controls['country_id'].setValue(
+      this.userdata[0]?.country_id
+    );
+    this.updatedForm.controls['address'].setValue(
+      this.userdata[0].address[0]?.address
+    );
+    // this.updatedForm.controls['address2'].setValue(
+    //   this.userdata[0].address[0]?.address2
+    // );
+    this.updatedForm.controls['city'].setValue(
+      this.userdata[0].address[0]?.city
+    );
+  }
 
-});
-
-// getuserdata() {
-//   this._AuthService.getuserdata().subscribe((res) => {
-//     this.userdata = res.data;
-//     this.imagediplay = res.data[0].image;
-//     this.onedit();
-//   });
-// }
-
-//edit for make data at input to update it
-onedit() {
-  this.updatedForm.controls['first_name'].setValue(
-    this.userdata[0]?.first_name
-  );
-  this.updatedForm.controls['last_name'].setValue(
-    this.userdata[0]?.last_name
-  );
-  this.updatedForm.controls['phone'].setValue(this.userdata[0]?.phone);
-  this.updatedForm.controls['email'].setValue(this.userdata[0]?.email);
-  this.updatedForm.controls['country_id'].setValue(
-    this.userdata[0]?.country_id
-  );
-
-  this.updatedForm.controls['address'].setValue(
-    this.userdata[0].address[0]?.address
-  );
-  // this.updatedForm.controls['address2'].setValue(
-  //   this.userdata[0].address[0]?.address2
-  // );
-  this.updatedForm.controls['city'].setValue(
-    this.userdata[0].address[0]?.city
-  );
-
-}
-
-updated(){
-
-    this._update.update("data:any").subscribe((res) => {
+  updated() {
+    this._update.update('data:any').subscribe((res) => {
       console.log(res);
-
-
-}
-
-)};
-
+    });
+  }
 }
