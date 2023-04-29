228f370979f75a2616b029d633bb2f1aa686ddb2
moshabrawy <mo.shabrawydev@gmail.com>
2023-04-29 09:55:20 +0200

    Medical Q&As

--- projects/user/src/app/views/comman/medicalqustion/medicalqustion.component.ts
+++ projects/user/src/app/views/comman/medicalqustion/medicalqustion.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-medicalqustion',
   templateUrl: './medicalqustion.component.html',
-  styleUrls: ['./medicalqustion.component.scss']
+  styleUrls: ['./medicalqustion.component.scss'],
 })
 export class MedicalqustionComponent {
+  ngAfterViewInit() {
+    const buttons = document.querySelectorAll('button');
 
+    buttons.forEach((button: any) => {
+
+      button.addEventListener('click', () => {
+        const parent = button.parentElement;
+        const siblings = parent.parentNode.children;
+
+        for (let i = 0; i < siblings.length; i++) {
+          if (siblings[i] !== parent) {
+            siblings[i].classList.remove('active');
+          }
+        }
+        button.parentElement.classList.toggle('active');
+      });
+    });
+  }
 }
