1cda354dbc1c633da52652c30ae62246174833a1
moshabrawy <mo.shabrawydev@gmail.com>
2023-06-13 02:07:02 +0200

    doctor

--- src/app/share/services/doctors.service.ts
+++ src/app/share/services/doctors.service.ts
@@ -4,18 +4,24 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { ActivatedRoute } from '@angular/router';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DoctorsService {
+<<<<<<< Updated upstream:src/app/share/services/doctors.service.ts
   id: any
   doctorDetails: any
   private  baseUrl = environment.baseUrl;
   constructor(private _http: HttpClient , private _ActivatedRoute: ActivatedRoute) { }
+=======
+  baseUrl = 'http://ydoctor.atwebpages.com/user/';
+  constructor(private _http: HttpClient) {}
+>>>>>>> Stashed changes:projects/user/src/app/share/services/doctors.service.ts
 
   allDoctors(): Observable<any> {
-    return this._http.get(this.baseUrl + `${'get_all_doctors'}`)
+    return this._http.get(this.baseUrl + `${'get_all_doctors'}`);
   }
 
+<<<<<<< Updated upstream:src/app/share/services/doctors.service.ts
   // sendId(id:number,slotId:number)
   //  {
 
@@ -26,19 +32,25 @@ export class DoctorsService {
 
 
   getDoctorsDetails(id:number): Observable<any> {
+=======
+  getDoctorsDetails(id: number): Observable<any> {
+>>>>>>> Stashed changes:projects/user/src/app/share/services/doctors.service.ts
     const data = {
       doctor_id: id,
-      details: true
-    }; return this._http.post(this.baseUrl + `${'get_doctor_details'}`, data)
-  }
-  allSpatialistsPage(page: number): Observable<any> {
-    return this._http.get(this.baseUrl + `get_all_specialties?page=${page}`)
-  }
-  allSpatialists(): Observable<any> {
-    return this._http.get(this.baseUrl + `${'get_all_specialties'}`)
-  }
-  search(data :any): Observable<any> {
-    return this._http.post(this.baseUrl + `${'search'}` , data)
+      details: true,
+    };
+    return this._http.post(this.baseUrl + `${'get_doctor_details'}`, data);
   }
 
+  allSpatialistsPage(page: number): Observable<any> {
+    return this._http.get(this.baseUrl + `get_all_specialties?page=${page}`);
+  }
+
+  allSpatialists(): Observable<any> {
+    return this._http.get(this.baseUrl + `${'get_all_specialties'}`);
+  }
+  
+  search(data: any): Observable<any> {
+    return this._http.post(this.baseUrl + `${'search'}`, data);
+  }
 }
