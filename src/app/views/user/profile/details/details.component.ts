4d7fafbd2b4c89cf7e3a5464bee369b5c57f1061
Mo Shabrawy <52353626+moshabrawy@users.noreply.github.com>
2023-06-14 12:32:55 +0300

    no posts

--- src/app/views/user/profile/details/details.component.ts
+++ src/app/views/user/profile/details/details.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-details',
@@ -7,4 +9,22 @@ import { Component, Input } from '@angular/core';
 })
 export class DetailsComponent {
   @Input() profileDetails: any;
+  constructor(private _router: Router, private elementRef: ElementRef, private renderer: Renderer2) { }
+
+  makeSettingsTabActive() {
+    const settingsTab = this.elementRef.nativeElement.querySelector('#settings');
+    console.log(settingsTab);
+    
+    this.renderer.addClass(settingsTab, 'show');
+    this.renderer.addClass(settingsTab, 'active');
+  }
+
+
+  navigateToSettingsTab() {
+    // Perform any necessary logic before navigating
+    this._router.navigateByUrl('/user/profile/settings');
+  }
+  // openNewTab() {
+  //   window.open('http://localhost:4200/user/profile/settings', '_self');
+  // }
 }
