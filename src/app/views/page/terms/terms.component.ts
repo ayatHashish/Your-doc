73661b04865328f0943d771066934cc19d39da65
ayatHashish <76957743+ayatHashish@users.noreply.github.com>
2023-06-17 16:15:58 +0300

    edit

--- src/app/views/page/terms/terms.component.ts
+++ src/app/views/page/terms/terms.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-terms',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./terms.component.scss']
 })
 export class TermsComponent {
+  pageUrl: String = ""
+  pageTitle: any = ""
 
+  constructor(private router: Router) {
+    router.events.pipe(
+      filter(res => res instanceof NavigationEnd)
+    ).subscribe(res => {
+      this.pageUrl = Object.values(res)[1];
+      this.pageTitle = this.pageUrl.split('/')[2];
+      // console.log(this.pageUrl);
+    });
+  }
 }
